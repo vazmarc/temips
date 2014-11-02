ab5d133a1bc19925465c7ec9000f3e5362a63668
Xiyu <sionwxy@gmail.com>
2014-11-02 00:06:02 -0400

    Adding test example.

---
+++ tests/example/test-Adder.t.v
@@ -0,0 +1,38 @@
+//==================================
+// Test bench
+//==================================
+
+`include "test-Adder.v"
+
+module test;
+   reg  [15:0] in0, in1;
+   reg         cin;
+   wire [15:0] out;
+   wire        cout;
+
+   test_Adder dut
+     (
+      .in0  (in0),
+      .in1  (in1),
+      .cin  (cin),
+      .out  (out),
+      .cout (cout)
+      );
+
+   initial begin
+      in0 = 1;
+      in1 = 1;
+      cin = 1;
+      
+      $display("Out = %x, Count = %x",
+	       out, cout);
+      #1;
+      $display("Out = %x, Count = %x",
+	       out, cout);
+      #5;
+      $display("Out = %x, Count = %x",
+	       out, cout);
+      
+   end // initial begin
+   
+endmodule // test
---
+++ tests/example/test-Adder.v
@@ -0,0 +1,20 @@
+//==================================
+// Test Module
+//==================================
+
+`ifndef TEST_ADDER_V
+`define TEST_ADDER_V
+
+module test_Adder (
+  input	  [15:0]  in0, in1,
+  input	          cin,
+  output  [15:0]  out,
+  output          cout
+);
+
+  assign {cout, out} = in0 + in1 + cin;
+
+endmodule
+
+`endif
+
